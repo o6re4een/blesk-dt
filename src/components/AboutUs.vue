b25e177b3a3d84dff74526b01e522aad92c388db
Sokolov Oleg <oleg-sokol.sokolov2016@yandex.ru>
2024-11-29 16:46:55 +0500

    AboutUs txt

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="about-us">
         <h2 class="subtitle about-us__title">О компании</h2>
-        <p class="about-us__txt">Мы – команда профессионалов, увлечённых идеей сделать ваш автомобиль 
+        <p class="about-us__txt txt">Мы – команда профессионалов, увлечённых идеей сделать ваш автомобиль 
             безупречно чистым и защищённым. 
         </p>
         <h2 class="subtitle about-us__subtitle">Почему выбирают нас:</h2>
         <ul class="about-us__list">
-            <li class="about-us__item">Опыт и мастерство.</li>
-            <li class="about-us__item">Высокое качество услуг.</li>
-            <li class="about-us__item">Индивидуальный подход.</li>
-            <li class="about-us__item">Комплексный уход.</li>
-            <li class="about-us__item">Гарантия удовлетворенности.</li>
-        </ul>
+            <li class="about-us__item txt">Опыт и мастерство.</li>
+            <li class="about-us__item txt">Высокое качество услуг.</li>
+            <li class="about-us__item txt">Индивидуальный подход.</li>
+            <li class="about-us__item txt">Комплексный уход.</li>
+            <li class="about-us__item txt">Гарантия удовлетворенности.</li>
+        </ul> 
         <h2 class="subtitle about-us__map">Мы на карте</h2>
         <div class="map-container">
             <div class="map"></div>
@@ -25,8 +25,6 @@
 </script>
 
 <style scoped>
-
-
 
 .about-us__txt {
     color: var(--shadows-color);
@@ -61,5 +59,27 @@
     border-radius: 20px;
 }
 
+@media screen and (max-width: 1024px) {
+    .txt {
+        font-size: 1.77rem;
+    }
+}
+@media screen and (max-width: 768px) {
+    .txt {
+        font-size: 1.57rem;
+    }
+}
+@media screen and (max-width: 576px) {
+    .txt {
+        font-size: 1.4rem;
+    }
+}
+
+
+@media screen and (max-width: 340px) {
+    .txt {
+        font-size: 0.82rem;
+    }
+}
 
 </style>
